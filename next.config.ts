c013f02cfd2a80dd38a8d803cc6ecaa0f13d7048
ThucDang <85912398+DangThuc202@users.noreply.github.com>
2025-03-24 14:31:03 +0700

    c

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,5 @@
-// import type { NextConfig } from "next";
-// /** @type {import('next').NextConfig} */
-
-// const nextConfig: NextConfig = {
-//   i18n: {
-//     locales: ["en", "vi"],
-//     defaultLocale: "vi",
-//   },
-
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ["en", "vi"],
-    defaultLocale: "vi",
-  },
-
   images: {
     unoptimized: true, // Không tối ưu hóa ảnh (cho phép dùng <img> mà không cảnh báo)
   },
